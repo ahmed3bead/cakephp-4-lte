21471f65eb5687dedcb87f7889a6229152fdffe8
Ahmed Ebead <ahmedm3bead@hotmail.com>
2021-06-28 07:31:28 +0200

    Signed-off-by: Ahmed Ebead <ahmedm3bead@hotmail.com>

--- templates/layout/lockscreen.php
+++ templates/layout/lockscreen.php
@@ -1,7 +1,10 @@
-<?php use Cake\Core\Configure; ?>
+<?php
+
+use Cake\Core\Configure; ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,13 +12,12 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <?php echo $this->Html->css('AdminLTE./bower_components/bootstrap/dist/css/bootstrap.min'); ?>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
   <!-- Font Awesome -->
-  <?php echo $this->Html->css('AdminLTE./bower_components/font-awesome/css/font-awesome.min'); ?>
+  <?php echo $this->Html->css('CakeLte./plugins/fontawesome-free/css/all.min.css'); ?>
   <!-- Ionicons -->
-  <?php echo $this->Html->css('AdminLTE./bower_components/Ionicons/css/ionicons.min'); ?>
   <!-- Theme style -->
-  <?php echo $this->Html->css('AdminLTE.AdminLTE.min'); ?>
+  <?php echo $this->Html->css('CakeLte./css/adminlte.min.css'); ?>
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -25,48 +27,50 @@
   <![endif]-->
 
   <!-- Google Font -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
   <?php echo $this->fetch('css'); ?>
 
 </head>
+
 <body class="hold-transition lockscreen">
-<!-- Automatic element centering -->
-<div class="lockscreen-wrapper">
-  <div class="lockscreen-logo">
-    <a href="<?php echo $this->Url->build(); ?>"><?php echo Configure::read('Theme.logo.large') ?></a>
-  </div>
-  <!-- User name -->
-  <div class="lockscreen-name">John Doe</div>
+  <!-- Automatic element centering -->
 
-  <!-- START LOCK SCREEN ITEM -->
-  <div class="lockscreen-item">
-    <!-- lockscreen image -->
-    <div class="lockscreen-image">
-      <?php echo $this->Html->image('user1-128x128.jpg', ['alt' => 'User Image']); ?>
+
+  <div class="lockscreen-wrapper">
+
+
+    <div class="lockscreen-logo">
+      <img width="150" src="<?php echo Configure::read('CakeLteTheme.logo.image') ?>" alt="<?php echo Configure::read('CakeLteTheme.title') ?>" class="src">
     </div>
-    <!-- /.lockscreen-image -->
 
-    <?php echo $this->fetch('content'); ?>
+  
+    <!-- User name -->
+    <div class="lockscreen-name">
+      <h1><?php echo Configure::read('CakeLteTheme.title') ?></h1>
+      <a href="<?php echo $this->Url->build('/admin'); ?>"><span>Admin Area</span></a> | 
+      <a href="<?php echo $this->Url->build('/call-center'); ?>"><span>Call Center</span></a> | 
+      <a href="#<?php //echo $this->Url->build('/technical-support'); ?>"><span>Technical Support</span></a>  
+
+      
+    </div>
+
+    <!-- START LOCK SCREEN ITEM -->
+    <div class="lockscreen-item">
+      <!-- lockscreen image -->
+
+
+    </div>
+    <!-- /.lockscreen-item -->
 
   </div>
-  <!-- /.lockscreen-item -->
-  <div class="help-block text-center">
-    Enter your password to retrieve your session
-  </div>
-  <div class="text-center">
-    <a href="<?php echo $this->Url->build('pages/examples/login'); ?>">Or sign in as a different user</a>
-  </div>
-  <div class="lockscreen-footer text-center">
-    Copyright &copy; 2014-2016 <b><a href="https://adminlte.io" class="text-black">Almsaeed Studio</a></b><br>
-    All rights reserved
-  </div>
-</div>
-<!-- /.center -->
+  <!-- /.center -->
 
-<!-- jQuery 3 -->
-<?php echo $this->Html->script('AdminLTE./bower_components/jquery/dist/jquery.min'); ?>
-<!-- Bootstrap 3.3.7 -->
-<?php echo $this->Html->script('AdminLTE./bower_components/bootstrap/dist/js/bootstrap.min'); ?>
+  <!-- /.center -->
+
+  <!-- jQuery 3 -->
+  <?php echo $this->Html->script('CakeLte./plugins/jquery/jquery.min.js'); ?>
+  <!-- Bootstrap 3.3.7 -->
+  <?php echo $this->Html->script('CakeLte./plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>
 </body>
+
 </html>
