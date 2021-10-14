4d17d6f60aaaae60c501fb82d38ea67bf4db8d5a
Ahmed Ebead <ahmedm3bead@hotmail.com>
2021-10-14 02:17:55 +0200

    Signed-off-by: Ahmed Ebead <ahmedm3bead@hotmail.com>

---
+++ templates/element/flash/success.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var array $params
+ * @var string $message
+ */
+if (!isset($params['escape']) || $params['escape'] !== false) {
+    $message = h($message);
+}
+?>
+<div id='flashMessage' class="message success" onclick="this.classList.add('hidden')"><?= $message ?></div>
+
+
